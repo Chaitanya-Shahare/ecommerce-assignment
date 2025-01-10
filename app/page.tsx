39c290e37426e5693618560fb8a86a7245c6bf55
Chaitanya Shahare <chaitanyashahare@Chaitanyas-MacBook-Air.local>
2025-01-10 10:57:38 +0530

    Get all products data

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,48 @@
 import { Card } from "@/components/card";
 import { Filter } from "@/components/filter";
 
-export default function Home() {
+// {
+//       id: 6,
+//       title: 'Huawei P30',
+//       description: 'Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.',
+//       price: 499,
+//       rating: 4.09,
+//       category: 'smartphones',
+//       image: 'https://i.dummyjson.com/data/products/5/1.jpg',
+//       __v: 0
+//     }
+
+export interface IProduct {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+  category: string;
+  image: string;
+  __v: number;
+}
+
+export default async function Home() {
+  let data = {
+    products: [],
+  };
+  try {
+    const response = await fetch(
+      "https://fake-ecommerce-app-api.onrender.com/products?limit=100&page=1&category=smartphones,laptops&minPrice=0&maxPrice=5000"
+    );
+    data = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+
   return (
     <div className="max-w-6xl mx-auto p-4 min-h-[60vh]">
       <div className="grid grid-cols-[1fr,2fr] gap-4">
         <Filter />
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          {Array.from({ length: 10 }).map((_, i) => (
-            <Card key={i} title="Product 1" price="$ 88" />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+          {data.products.map((product: IProduct, i: number) => (
+            <Card key={i} title={product.title} price={product.price} />
           ))}
         </div>
       </div>
--- components/card.tsx
+++ components/card.tsx
@@ -1,9 +1,9 @@
-export const Card = ({ title, price }: { title: string; price: string }) => {
+export const Card = ({ title, price }: { title: string; price: number }) => {
   return (
-    <div className="p-1 w-44 hover:bg-gray-100">
+    <div className="p-2  hover:bg-gray-100">
       <div className="h-48  bg-gray-300"></div>
-      <h3 className="text-lg font-bold mt-2">{title}</h3>
-      <p className="">{price}</p>
+      <h3 className="text-lg font-bold mt-2 line-clamp-1">{title}</h3>
+      <p className="">$ {price}</p>
     </div>
   );
 };
